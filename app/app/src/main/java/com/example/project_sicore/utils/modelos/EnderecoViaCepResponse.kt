656bb8e5a94b7c6d82766ebb4cacbcd7e8353024
Djonathan Briesch <djonathanvinicius@gmail.com>
2024-06-19 21:13:07 -0300

    SOCORRO

--- app/app/src/main/java/com/example/project_sicore/utils/modelos/EnderecoViaCepResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.project_sicore.utils.modelos
-
-class EnderecoViaCepResponse {
-    val cep: String = ""
-    val uf: String = ""
-    val localidade: String = ""
-    val bairro: String = ""
-    val logradouro: String = ""
-}
